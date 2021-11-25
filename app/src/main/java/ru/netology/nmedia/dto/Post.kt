aedfd62ae48a02676f1ad74d540abc09606c22ae
ga1duk <gaidukdg@yandex.ru>
2021-11-25 19:41:35 +0300

    likeById and dislikeById functions were made. App works correctly. Homework likes is prepared.

--- app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -5,7 +5,7 @@ data class Post(
     val author: String,
     val content: String,
     val published: String,
-    var likedByMe: Boolean,
-    var likes: Int = 0
+    val likedByMe: Boolean,
+    val likes: Int = 0
 )
 
--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -4,8 +4,8 @@ import ru.netology.nmedia.dto.Post
 
 interface PostRepository {
     fun getAll(): List<Post>
-    fun likeById(id: Long)
-    fun dislikeById(id: Long)
+    fun likeById(id: Long): Post
+    fun dislikeById(id: Long): Post
     fun save(post: Post)
     fun removeById(id: Long)
 }
