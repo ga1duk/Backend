ec328b95354fa9453c08c98d3b6788c9de694eec
ga1duk <gaidukdg@yandex.ru>
2021-12-06 00:12:34 +0300

    PostsApiService was added. All the functions in PostRepositoryImpl.kt were modified using retrofit.

---
+++ app/src/main/java/ru/netology/nmedia/api/PostsApiService.kt
@@ -0,0 +1,51 @@
+package ru.netology.nmedia.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
+import ru.netology.nmedia.BuildConfig
+import ru.netology.nmedia.dto.Post
+
+private const val BASE_URL = "${BuildConfig.BASE_URL}/api/"
+
+val logging = HttpLoggingInterceptor().apply {
+    if(BuildConfig.DEBUG) {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+}
+
+private val okhttp = OkHttpClient.Builder()
+    .addInterceptor(logging)
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .client(okhttp)
+    .baseUrl(BASE_URL)
+    .build()
+
+interface PostsApiService {
+    @GET("posts")
+    fun getAll(): Call<List<Post>>
+
+    @POST("posts/{id}/likes")
+    fun likeById(@Path("id") id: Long): Call<Post>
+
+    @DELETE("posts/{id}/likes")
+    fun dislikeById(@Path("id") id: Long): Call<Post>
+
+    @POST("posts")
+    fun save(@Body post: Post): Call<Post>
+
+    @DELETE("posts/{id}")
+    fun removeById(@Path("id") id: Long): Call<Unit>
+}
+
+object PostApi {
+    val retrofitService: PostsApiService by lazy {
+        retrofit.create(PostsApiService::class.java)
+    }
+}
