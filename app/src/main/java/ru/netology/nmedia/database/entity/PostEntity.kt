cb9e43b6d6fcc1d7138727b71d2db642560de06e
ga1duk <gaidukdg@yandex.ru>
2022-05-05 00:10:29 +0300

    All the code for posting images to server was added. ImagePicker library was added. Ability to pick photos from gallery, or take photos with camera was added

--- app/src/main/java/ru/netology/nmedia/database/entity/PostEntity.kt
+++ app/src/main/java/ru/netology/nmedia/database/entity/PostEntity.kt
@@ -1,8 +1,10 @@
 package ru.netology.nmedia.database.entity
 
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import ru.netology.nmedia.dto.Attachment
+import ru.netology.nmedia.dto.AttachmentType
 import ru.netology.nmedia.dto.Post
 
 @Entity
@@ -15,17 +17,31 @@ data class PostEntity(
     val published: String,
     val likedByMe: Boolean,
     val likes: Int = 0,
-    val attachment: Attachment? = null,
+    @Embedded
+    var attachment: AttachmentEmbeddable?,
     var isPostVisible: Boolean = true
 ) {
-    fun toDto() = Post(id, author, authorAvatar, content, published, likedByMe, likes, attachment)
+    fun toDto() = Post(id, author, authorAvatar, content, published, likedByMe, likes, attachment?.toDto())
 
     companion object {
         fun fromDto(dto: Post) =
             PostEntity(
                 dto.id, dto.author, dto.authorAvatar, dto.content, dto.published,
-                dto.likedByMe, dto.likes, dto.attachment
+                dto.likedByMe, dto.likes, AttachmentEmbeddable.fromDto(dto.attachment)
             )
+    }
+}
+
+data class AttachmentEmbeddable(
+    var url: String,
+    var type: AttachmentType
+) {
+    fun toDto() = Attachment(url, type)
+
+    companion object {
+       fun fromDto(dto: Attachment?) = dto?.let {
+           AttachmentEmbeddable(it.url, it.type)
+       }
     }
 }
 
--- app/src/main/java/ru/netology/nmedia/database/typeconverter/AttachmentConverter.kt
+++ app/src/main/java/ru/netology/nmedia/database/typeconverter/AttachmentConverter.kt
@@ -1,17 +1,17 @@
-package ru.netology.nmedia.database.typeconverter
-
-import androidx.room.TypeConverter
-import ru.netology.nmedia.dto.Attachment
-
-class AttachmentConverter {
-
-    @TypeConverter
-    fun attachmentToString(attachment: Attachment?): String? {
-        return attachment?.url
-    }
-
-    @TypeConverter
-    fun stringToAttachment(url: String?): Attachment? {
-        return if (url != null) Attachment(url = url) else null
-    }
-}
+//package ru.netology.nmedia.database.typeconverter
+//
+//import androidx.room.TypeConverter
+//import ru.netology.nmedia.dto.Attachment
+//
+//class AttachmentConverter {
+//
+//    @TypeConverter
+//    fun attachmentToString(attachment: Attachment?): String? {
+//        return attachment?.url
+//    }
+//
+//    @TypeConverter
+//    fun stringToAttachment(url: String?): Attachment? {
+//        return if (url != null) Attachment(url = url) else null
+//    }
+//}
